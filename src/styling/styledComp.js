a2ab410ccadb071333e3b8cc26949fc6ee17db40
tobiasengberg <tobias.engberg@exilic.se>
2024-09-11 10:11:23 +0200

    Draft edit functionality

---
+++ src/styling/styledComp.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const BookList = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 0.8rem 0;
+    & p {
+        padding: 0 1rem;
+    }
+`;
